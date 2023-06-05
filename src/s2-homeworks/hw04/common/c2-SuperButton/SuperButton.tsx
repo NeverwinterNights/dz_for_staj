dc515ab96f7a84b21378ab0d4b3e511b156c7ac4
Pavel Kardash <cardash@mail.ru>
2023-06-05 20:53:52 +0300

    done4 fix2

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -24,7 +24,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     //             ? s.red : ""}`
     //     + `${className ? ' ' + className : ''}` // задачка на смешивание классов
 
-     const finalClassName = s.button  + ` ${disabled ?  s.disabled : xType == "red" ? s.red : xType == "secondary" ? s.secondary : ""} ${s.default}`
+     const finalClassName = s.button  + ` ${disabled ?  s.disabled : xType == "red" ? s.red : xType == "secondary" ? s.secondary : ""}  ${s.default}`
     // const finalClassName = `${disabled ? s.disabled : xType == "red" ? `${s.red} ${s.default}` : s.default} ${className? ' ' + className : ''}`
 
     // const finalClassName = s.button
